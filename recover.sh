e43b6d20189f14514ef8c8cca4c5fdbc0019ca95
racterub <vivi.450@hotmail.com>
2019-06-11 23:51:48 +0800

    add recover script

---
+++ recover.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ -f docker-compose.yml ]; then
+    rm docker-compose.yml;
+else
+    echo "Docker-compose config not found, this needs to recover manually";
+    exit;
+fi
+
+for d in `find chal/* -type d -maxdepth 0`; do
+    for f in `find ${d}/* -type f -maxdepth 0`; do
+        rm ${f};
+    done;
+    mv ${d}/bin/* ${d}/;
+    rm -r ${d}/bin;
+done;
